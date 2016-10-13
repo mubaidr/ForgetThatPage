8e59f2d37bcd3e5b1fa2655c061e4a491cf653c9
cyrilweller <cyril.weller@protonmail.com>
2016-10-13 19:26:29 +0200

    Added blacklist checkbox in options

--- firefox/js/options.js
+++ firefox/js/options.js
@@ -13,9 +13,11 @@
 var cookieMessage = chrome.i18n.getMessage("cookieMessage");
 var localStorageMessage = chrome.i18n.getMessage("localStorageMessage");
 var sessionStorageMessage = chrome.i18n.getMessage("sessionStorageMessage");
+var blacklistMessage = chrome.i18n.getMessage("blacklistMessage");
 document.getElementById("cookieMessage").innerHTML = cookieMessage;
 document.getElementById("localStorageMessage").innerHTML = localStorageMessage;
 document.getElementById("sessionStorageMessage").innerHTML = sessionStorageMessage;
+document.getElementById("blacklistMessage").innerHTML = blacklistMessage;
 
 // Saves options to chrome.storage.sync.
 function save_options() {
@@ -23,11 +25,13 @@ function save_options() {
   var cookies = document.getElementById('cookies').checked;
   var localStorage = document.getElementById('localStorage').checked;
   var sessionStorage = document.getElementById('sessionStorage').checked;
+  var blacklistStorage = document.getElementById('blacklistMessage').checked;
 
   chrome.storage.local.set({
     cookies: cookies,
     localStorage: localStorage,
-    sessionStorage: sessionStorage
+    sessionStorage: sessionStorage,
+    blacklistStorage: blacklistStorage
   }, function() {
 
     // Update status to let user know options were saved.
@@ -48,11 +52,13 @@ function restore_options() {
   chrome.storage.local.get({
     cookies: true,
     localStorage: true,
-    sessionStorage: true
+    sessionStorage: true,
+    blacklistStorage: true
   }, function(items) {
     document.getElementById('cookies').checked = items.cookies ;
     document.getElementById('localStorage').checked = items.localStorage ;
     document.getElementById('sessionStorage').checked = items.sessionStorage ;
+    document.getElementById('blacklistStorage').checked = items.blacklistStorage ;
   });
 
 }
